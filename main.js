1fd03d0e017891b2ed061d6f4544533d47b9eff0
Mason Wudtee <mason.wudtee@gmail.com>
2022-09-16 15:22:21 -0500

    adding helpful comments to JS file

--- main.js
+++ main.js
@@ -15,17 +15,57 @@
 // search box, or as soon as they select an option from the select.
 
 
-function renderCoffee(coffee) {
-    let html = '<tr class="coffee">';
-    html += '<td>' + coffee.id + '</td>';
-    html += '<td>' + coffee.name + '</td>';
-    html += '<td>' + coffee.roast + '</td>';
-    html += '</tr>';
+// ############################################ MY CODE ####################
+// function renderCoffee(coffee) {
+//     let html = '<div class="coffee">';
+//     html += '<p>' + coffee.id + '</p>';
+//     html += '<h1>' + coffee.name + '</h1>';
+//     html += '<p>' + coffee.roast + '</p>';
+//     html += '</div>';
+//
+//     return html;
+// }
+
+
+// ################################### ORIGINAL CODE ################################
+//function renderCoffee(coffee) {
+//     var html = '<tr class="coffee">';
+//     html += '<td>' + coffee.id + '</td>';
+//     html += '<td>' + coffee.name + '</td>';
+//     html += '<td>' + coffee.roast + '</td>';
+//     html += '</tr>';
+//
+//     return html;
+// }
+
+                        //let coffees = [
+                        //     {id: 1, name: 'Light City', roast: 'light'},
+                        //     {id: 2, name: 'Half City', roast: 'light'},
+                        //     {id: 3, name: 'Cinnamon', roast: 'light'},
+                        //     {id: 4, name: 'City', roast: 'medium'},
+                        //     {id: 5, name: 'American', roast: 'medium'},
+                        //     {id: 6, name: 'Breakfast', roast: 'medium'},
+                        //     {id: 7, name: 'High', roast: 'dark'},
+                        //     {id: 8, name: 'Continental', roast: 'dark'},
+                        //     {id: 9, name: 'New Orleans', roast: 'dark'},
+                        //     {id: 10, name: 'European', roast: 'dark'},
+                        //     {id: 11, name: 'Espresso', roast: 'dark'},
+                        //     {id: 12, name: 'Viennese', roast: 'dark'},
+                        //     {id: 13, name: 'Italian', roast: 'dark'},
+                        //     {id: 14, name: 'French', roast: 'dark'},
+                        // ];
+
+function renderCoffee(coffee) { // Adds divs for every item in coffees variable
+    let html = '<div class="coffee">';
+    // html += '<p>' + coffee.id + '</p>';
+    html += '<h1>' + coffee.name + '</h1>';
+    html += '<p>' + coffee.roast + '</p>';
+    html += '</div>';
 
     return html;
 }
 
-function renderCoffees(coffees) {
+function renderCoffees(coffees) { // This chooses how to display/ what order the coffees will be displayed
     let html = '';
     for(let i = 0; i < coffees.length; i++) {
         html += renderCoffee(coffees[i]);
@@ -35,8 +75,8 @@ function renderCoffees(coffees) {
 
 function updateCoffees(e) {
     e.preventDefault(); // don't submit the form, we just want to update the data
-    let selectedRoast = roastSelection.value;
-    let filteredCoffees = [];
+    let selectedRoast = roastSelection.value; // whether it's dark, light, medium etc
+    let filteredCoffees = []; // filtered coffees is the new array that will display the user-input values
     coffees.forEach(function(coffee) {
         if (coffee.roast === selectedRoast) {
             filteredCoffees.push(coffee);
