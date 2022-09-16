9822a72d781f1b45b26da8b79b4f4b3854f967db
Mason Wudtee <mason.wudtee@gmail.com>
2022-09-16 14:36:43 -0500

    new branch addition

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
 "use strict"
 
+//Update the HTML
+//
+// Tables are a little old school, you need to refactor the code so that each coffee is displayed
+// in a div that contains a heading displaying the coffee name, and the type of roast in a paragraph.
+// Don't display the ids, these are only for our application's internal use
+//
+// When the page loads, the coffees should be sorted by their ids in ascending order
+//
+// Add functionality to search through the coffees by name,
+// and display only the coffees that match the provided search term (You will need to add an input field to the existing form for this)
+//
+// Add functionality to update the displayed coffee as the user types into the
+// search box, or as soon as they select an option from the select.
+
+
 function renderCoffee(coffee) {
-    var html = '<tr class="coffee">';
+    let html = '<tr class="coffee">';
     html += '<td>' + coffee.id + '</td>';
     html += '<td>' + coffee.name + '</td>';
     html += '<td>' + coffee.roast + '</td>';
@@ -11,8 +26,8 @@ function renderCoffee(coffee) {
 }
 
 function renderCoffees(coffees) {
-    var html = '';
-    for(var i = coffees.length - 1; i >= 0; i--) {
+    let html = '';
+    for(let i = 0; i < coffees.length; i++) {
         html += renderCoffee(coffees[i]);
     }
     return html;
@@ -20,8 +35,8 @@ function renderCoffees(coffees) {
 
 function updateCoffees(e) {
     e.preventDefault(); // don't submit the form, we just want to update the data
-    var selectedRoast = roastSelection.value;
-    var filteredCoffees = [];
+    let selectedRoast = roastSelection.value;
+    let filteredCoffees = [];
     coffees.forEach(function(coffee) {
         if (coffee.roast === selectedRoast) {
             filteredCoffees.push(coffee);
@@ -31,7 +46,7 @@ function updateCoffees(e) {
 }
 
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
-var coffees = [
+let coffees = [
     {id: 1, name: 'Light City', roast: 'light'},
     {id: 2, name: 'Half City', roast: 'light'},
     {id: 3, name: 'Cinnamon', roast: 'light'},
@@ -48,9 +63,11 @@ var coffees = [
     {id: 14, name: 'French', roast: 'dark'},
 ];
 
-var tbody = document.querySelector('#coffees');
-var submitButton = document.querySelector('#submit');
-var roastSelection = document.querySelector('#roast-selection');
+
+
+let tbody = document.querySelector('#coffees');
+let submitButton = document.querySelector('#submit');
+let roastSelection = document.querySelector('#roast-selection');
 
 tbody.innerHTML = renderCoffees(coffees);
 
